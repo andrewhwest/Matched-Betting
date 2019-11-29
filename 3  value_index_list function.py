9a8859c32fb07b0a92fa37536a8338ae2cb4f3d6
Andrew West <57451860+andrewhwest@users.noreply.github.com>
2019-11-29 14:33:06 +0000

    Update 3  value_index_list function.py
    Explanation of the generalised syntax

--- 3  value_index_list function.py
+++ 3  value_index_list function.py
@@ -1,3 +1,6 @@
+"""Require this function to calculate account balances for each bookmaker. 
+   Generalised its syntax as it could be extremely useful and commonly used for work with future dataframes."""
+
 def value_index_list(series):
     """Iterates through values in a pandas series and returns a dictionary of the form
        {value : [indices for value]}"""
