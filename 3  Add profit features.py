fe30d8b51f88b113c8d8246dfbdd114df443bbe3
Andrew West <ahwest@yahoo.com>
2019-12-03 11:24:16 +0000

    Made functions from part 3 to be used in part 4.

--- 3  Add profit features.py
+++ 3  Add profit features.py
@@ -6,13 +6,9 @@ from balance_functions import *
 
 bet_spreadsheet = pd.read_csv('Bet Spreadsheet with missing bets added.csv')
 
-# Calculate profit from bet result
-win_lose_boolean = (bet_spreadsheet['Bet Result'] == 'Win')
-bet_profit = bet_spreadsheet['Potential Profit'] * win_lose_boolean
-
-# Add profit as a column 
-column_after_Bet_Result = bet_spreadsheet.columns.get_loc('Bet Result') + 1
-bet_spreadsheet.insert(column_after_Bet_Result, 'Profit', bet_profit)
+# Add profit
+bet_spreadsheet = add_profit_column(bet_spreadsheet)
+bet_spreadsheet = calculate_profit(bet_spreadsheet)
 
 # Tidy up note on rows 12 and 13
 bet_spreadsheet.loc[[12,13], 'Note'] = '''Liability changed last minute to 118.01, 
@@ -27,12 +23,9 @@ bet_spreadsheet.loc[2, 'Profit'] = 2.57
 bet_spreadsheet.loc[[6,7], 'Note'] = '7.33 of losses from exchange refunded'
 bet_spreadsheet.loc[6, 'Profit'] = 7
 
-# Remove minus from -0.00 in profit
-bet_spreadsheet.iloc[:, -3] = bet_spreadsheet.iloc[:, -3].apply(lambda x : abs(x) 
-                                                                if (x == -0.00) else x)
-# Add Running Profit as a column
-column_after_Profit = column_after_Bet_Result + 1 
-bet_spreadsheet.insert(column_after_Profit, 'Running Profit', bet_spreadsheet['Profit'].cumsum())
+# Add running profit
+bet_spreadsheet = remove_negative_zeros(bet_spreadshet)
+bet_spreadsheet = running_profit(bet_spreadsheet)
 
 # Show balances for each bookmaker
 bookie_balances(bet_spreadsheet)
